842086a5de00ae3dba7b09d1c84bd669be2f4ebb
Anne Kwok <guomulian@gmail.com>
2019-10-28 04:02:06 -0400

    Add energy exit option to BaseAnnealer, generate_filename helper, and sudoku test

--- anneal/helpers.py
+++ anneal/helpers.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import time
 
 
 def clip(item, lower, upper):
@@ -13,3 +14,12 @@ def distance(p1, p2):
     p2 = np.array(p2)
 
     return np.linalg.norm(p1 - p2)
+
+
+def generate_filename(obj, extension):
+    """Generates a timestamp-based filename prefixed with the class name of the
+    given object.
+    """
+    timestamp = time.strftime("-%Y%m%d-%H%M%S")
+    filename = obj.__class__.__name__ + timestamp + extension
+    return filename
